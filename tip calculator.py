01e08028f5dbd75a0d6435eb3746dcc6c57e9a25
Robert Solomon <85691492+Robert-max-byte@users.noreply.github.com>
2023-05-30 13:31:03 +0000

    Update tip calculator.py

--- tip calculator.py
+++ tip calculator.py
@@ -1,1 +1,10 @@
+print("Welcome to Berto tip calculator")
+bill =int(input("Enter the bill?"))
+tip = int(input("Enter the percentage of tip you want to pay 10,12 or 15?"))
+people=int(input("How many people to split the bill?"))
 
+totalTip=(tip/100)+1
+totalBill=(bill/people) * totalTip
+finalBill=round(totalBill,3)
+
+print(f"Each person will pay {finalBill} amount")
